dfe1e6bd863611354e80a343a97b97447f04a580
claudioc0 <165798717+claudioc0@users.noreply.github.com>
2024-10-22 11:58:35 -0300

    Insertion Sort

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         HashTable table = new HashTable(10);
+        HashTable table2 = new HashTable(10);
+
+        table2.insert(22);
+        table2.insert(39);
+        table2.insert(42);
+        table2.insert(19);
+        table2.insert(1);
+        table2.insert(88);
+        table2.insert(23);
+        table2.insert(4);
+        table2.insert(332);
+
+        System.out.println(table2);
+
+        table2.insertionSort();
+        System.out.println(table2 + "\n");
 
         table.insert(12);
         table.insert(25);
@@ -21,7 +37,7 @@ public class Main {
 
         System.out.println(table.remove(12));
 
-        table.bubbleSort();
+        table.insertionSort();
         System.out.println(table);
 
     }
